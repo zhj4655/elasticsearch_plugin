8d5fd045aa55f765959ea61b8c3461dd52374309
yduanqiaocanxueD <837478687@qq.com>
2018-09-22 11:27:48 +0800

    update variable name

--- src/main/java/com/taste/elasticsearch_taste/grpc/FormatGrpcData.java
+++ src/main/java/com/taste/elasticsearch_taste/grpc/FormatGrpcData.java
@@ -9,12 +9,12 @@ public class FormatGrpcData {
 
     //格式化数据，将string list转换为ConnRequest list
     public static List<ConnRequest> formatDate(String type,List<String> datas){
-        List<ConnRequest> Fdatas = new ArrayList<>();
-        Fdatas.add(ConnRequest.newBuilder().setRequest(type).build());
+        List<ConnRequest> fDatas = new ArrayList<>();
+        fDatas.add(ConnRequest.newBuilder().setRequest(type).build());
         for(String data : datas){
-            Fdatas.add(ConnRequest.newBuilder().setRequest(data).build());
+            fDatas.add(ConnRequest.newBuilder().setRequest(data).build());
         }
-        return Fdatas;
+        return fDatas;
     }
     //格式化一条数据，将String转换为ConnRequest
     public static ConnRequest formatOneData(String data){
